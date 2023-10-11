09e630e9b07cbe2d4270195c956f66d8ca2d4706
Bhuvansh <bhuvanshgoyal2004@gmail.com>
2023-10-11 11:49:22 +0530

    chore: remove question level

--- src/utils/custom_logger.py
+++ src/utils/custom_logger.py
@@ -5,17 +5,10 @@ from colorlog import ColoredFormatter
 ALERT = 25 
 logging.addLevelName(ALERT, "ALERT")  
 
-QUESTION = 26
-logging.addLevelName(QUESTION, "QUESTION")
-
 class CustomLogger(logging.Logger):
     def alert(self, message, *args, **kwargs):
         if self.isEnabledFor(ALERT):
             self._log(ALERT, message, args, **kwargs)
-    
-    def question(self, message, *args, **kwargs):
-        if self.isEnabledFor(QUESTION):
-            self._log(QUESTION, message, args, **kwargs)
 
 # color formatting
 class ColoredFormatter(logging.Formatter):
@@ -26,7 +19,6 @@ class ColoredFormatter(logging.Formatter):
         'ERROR': "\033[1;31m",        # Red
         'CRITICAL': "\033[1;41;97m",  # White on Red 
         'ALERT': "\033[1;35m",        # Magenta 
-        'QUESTION': "\033[1;34m",     # Blue 
         'RESET': "\033[0m"            # Reset to default colors
     }
     def format(self, record):
